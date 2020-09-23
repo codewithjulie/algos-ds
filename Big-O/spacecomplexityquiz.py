02cce47c5f2b3268e8be2aa91fc2ce2ee28ca468
codewithjulie <codewithjulie@gmail.com>
2020-09-23 23:51:48 +0000

    update quiz

--- Big-O/spacecomplexityquiz.py
+++ Big-O/spacecomplexityquiz.py
@@ -1,23 +1,23 @@
-Space complexity: How much additional memory do we need to allocate in order to run the code in our algorithm (auxiliary space complexity)
-not worried about space taken up by the inputs
+Space complexity: How much additional memory do we need to allocate in order to run the code in our algorithm (auxiliary space complexity) note: not worried about space taken up by the inputs
 
-Which of the following are constant space?
-a. strings
-b. booleans
-c. numbers
-d. undefined
-e. array of 5 elements
+1. Which of the following are constant space?
+    a. strings
+    b. booleans
+    c. numbers
+    d. undefined
+    e. array of 5 elements
+    f. empty array
 
-What is the space complexity of the following:
-for i in range(n):
-    print(i + i)
+2. What is the space complexity of the following:
+    a.  for i in range(n):
+            print(i + i)
 
-What is the space complexity of the following:
-new_array = []
-for i in range(n):
-    new_array.append(i)
 
-What is the space complexity of the following:
-for i in range(n):
-    for j in range(n):
-        return i * j
+    b.  new_array = []
+        for i in range(n):
+            new_array.append(i)
+
+
+    c.  for i in range(n):
+            for j in range(n):
+                return i * j
--- Big-O/timecomplexityquiz.py
+++ Big-O/timecomplexityquiz.py
@@ -1,35 +1,35 @@
-Which of the following are constant in regards to time complexity?
-a. variable assignments
-b. accessing an element in an array by index
-c. arithmetic operations
-d. in a loop where there are only arithmetic operations inside the loop
-e. searching for an element in a linked list
-f. searching for an element in an array
+1. Which of the following are constant in regards to time complexity?
+    a. variable assignments
+    b. accessing an element in an array by index
+    c. searching for an element in a linked list
+    d. in a loop where there are only arithmetic operations inside the loop
+    e. arithmetic operations
+    f. searching for an element in an array
 
-How could you simplify the big O expression of each of the following?
-1. O(n + 10)
-2. O(2n)
-3. O(1000n + 500n)
-4. O(5n^2 + 100n + 25)
-5. O(1000)
-6. O(n^2 + n^3)
+2. How could you simplify the big O expression of each of the following?
+    a. O(n + 10)
+    b. O(2n)
+    c. O(1000n + 500n)
+    d. O(5n^2 + 100n + 25)
+    e. O(1000)
+    f. O(n^2 + n^3)
 
-What is the time complexity of the following:
-for i in range(5000):
-    print(i + i)
+3. What is the time complexity of the following:
+    a. for i in range(5000):
+            print(i + i)
 
-What is the time complexity of the following:
-for i in range(n):
-    print(i + i)
 
-What is the time complexity of the following:
-for i in range(n):
-    for j in range(n):
-        return i * j
+    b. for i in range(n):
+            print(i + i)
 
-Place the following big O expressions in order from fast to slow:
-* O(log n)
-* O(n)
-* O(1)
-* O(n^2)
+
+    c. for i in range(n):
+          for j in range(n):
+              return i * j
+
+4. Place the following big O expressions in order from fast to slow:
+    a. O(log n)
+    b. O(n)
+    c. O(1)
+    d. O(n^2)
 
